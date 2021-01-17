59e80ce9b5d33cf8411157d98e9163b50a59c922
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2021-01-17 08:56:02 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  var strP = '<br>';
-  var tmpStr =''
-  var q = req.query;
-  for (var a in q){
-    tmpStr = 'Parametr w url: ' + a + ' wartość: ' + q[a];
-    console.log(tmpStr);
-    strP += '<br>' + tmpStr
-  }
-  res.send('Hello World! on Heroku platform :) ' +
-  '<br>Deploy from Github details: <b>piotrektekkno/herokuNodeJS </b>' +
-  strP);
+const http = require('http');
+const port = process.env.PORT || 8081
 
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
 
-});
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const server = http.createServer(requestListener);
+server.listen(port);

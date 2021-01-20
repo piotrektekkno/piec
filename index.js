bdb9f60c786a4e89f34721ca903d8789ac38081d
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2021-01-20 20:59:49 +0100

    test port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8081
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hello, World! ' + process.env.PORT );
 }
 
 const server = http.createServer(requestListener);

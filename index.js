245361ff7b10d023d877f1dc3f4316fda206567f
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2021-04-08 17:57:09 +0200

    400 head

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 8081
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
+  res.writeHead(400);
   res.end('Hello, World! ' + process.env.PORT );
 }
 

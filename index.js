b068d20944a9b0d9fc2fa73264cec340920ca881
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2021-04-08 18:10:44 +0200

    DTTM

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require('http');
 const port = process.env.PORT || 8081
 
 const requestListener = function (req, res) {
+  let dt = new Date;
   res.writeHead(200);
-  res.end('Hello, World! ' + process.env.PORT );
+  res.end('Hello, World! ' + process.env.PORT + ' ' + dt.getTime() );
+
 }
 
 const server = http.createServer(requestListener);
